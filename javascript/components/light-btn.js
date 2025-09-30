7fe4fc0535e184c8acea2b1988d5ed4dfb3bc9ff
4lex_16 <cirlana16@outlook.com>
2025-09-29 23:27:43 -0400

    main: [HTML, CSS, JS] Added new div, discovered hiccup, added comments HTML -> Added new div window-content to each window CSS -> Discovered :root colors were backwards JS -> Added Comments to Lighting Buttons

--- javascript/components/light-btn.js
+++ javascript/components/light-btn.js
@@ -3,29 +3,33 @@ export function LightBtn() {
     const root = document.documentElement;
     const lightingBtn = document.querySelector("#lightingBtn");
 
+    // Sets dark mode based on client's computer settings
     if (window && window.matchMedia && window.matchMedia('prefer-color-scheme: dark')) {
         root.classList.add("dark");
         lightingBtn.innerHTML = `<img src="/assets/pngs/moon.png" alt="moon jpg"/>`;
     }
 
+    // Sets dark mode based on localStorage
     if (localStorage && localStorage.getItem("lighting")) {
         if (localStorage.getItem("lighting") === "dark_mode") {
             root.classList.add("dark");
             lightingBtn.innerHTML = `<img src="/assets/pngs/moon.png" alt="moon jpg"/>`;
-        } else {
-            root.classList.add("light");
+        }
+        else if (localStorage.getItem("lighting") === "light_mode"){
+            root.classList.remove("dark");
             lightingBtn.innerHTML = `<img src="/assets/pngs/sun.png" alt="sun jpg"/>`;
         }
     }
 
+    // Changes lighting mode
     lightingBtn.addEventListener("click", () => {
         root.classList.toggle("dark");
-        if (lightingBtn.classList.toggle("sun-btn")) {
-            lightingBtn.innerHTML = `<img src="/assets/pngs/moon.png" alt="sun jpg"/>`;
+        if (!root.classList.contains("dark")) {
+            lightingBtn.innerHTML = `<img src="/assets/pngs/sun.png" alt="sun jpg"/>`;
             localStorage.setItem("lighting", "light_mode")
         }
-        if (lightingBtn.classList.toggle("moon-btn")) {
-            lightingBtn.innerHTML = `<img src="/assets/pngs/sun.png" alt="moon jpg"/>`;
+        if (root.classList.contains("dark")) {
+            lightingBtn.innerHTML = `<img src="/assets/pngs/moon.png" alt="moon jpg"/>`;
             localStorage.setItem("lighting", "dark_mode")
         }
     })
